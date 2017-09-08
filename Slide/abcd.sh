e86e1085ca3c98ccf1712d44d9ce27b8335f7161
a6 <Ansal>
2017-09-08 22:58:13 +0530

    first

---
+++ Slide/abcd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script asks the user for a time, waits the specified amount
+# of time, and shows an alert dialog.
+
+TIME=$(zenity --entry --title="Timer" --text="Enter a duration for the timer.\n\n Use 5s for 5 seconds, 10m for 10 minutes, or 2h for 2 hours.")
+
+sleep ${TIME}
+
+cvlc --play-and-exit sound # Alarm
+
+zenity --info --title="Timer Complete" --text="The timer is over."
